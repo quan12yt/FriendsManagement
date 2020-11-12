621f728f9ce14d8b8af17e227ab067ba557f24ab
Quan Pham <quan12yt@gmail.com>
2020-11-12 17:02:00 +0700

    task 4 and 5 service layer unit test completed (successed case)

--- src/test/java/unittest/EmailServiceImpTest.java
+++ src/test/java/unittest/EmailServiceImpTest.java
@@ -5,32 +5,28 @@ import com.example.demo.model.Email;
 import com.example.demo.model.FriendRelationship;
 import com.example.demo.payload.AddAndGetCommonRequest;
 import com.example.demo.payload.EmailRequest;
+import com.example.demo.payload.RetrieveRequest;
+import com.example.demo.payload.SubscribeAndBlockRequest;
 import com.example.demo.repository.EmailRepository;
 import com.example.demo.repository.FriendRelationshipRepository;
-import com.example.demo.service.EmailService;
 import com.example.demo.service.serviceimp.EmailServiceImp;
 import com.example.demo.utils.FriendStatus;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.stubbing.Answer;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.validation.constraints.AssertTrue;
 import java.util.*;
 import java.util.stream.Collectors;
 
+import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest(classes = FriendManagementApplication.class)
@@ -41,7 +37,6 @@ public class EmailServiceImpTest {
 
     @InjectMocks
     private EmailServiceImp emailService;
-
     @Mock
     private EmailRepository emailRepository;
     @Mock
@@ -49,6 +44,8 @@ public class EmailServiceImpTest {
 
     private EmailRequest emailRequest;
     private AddAndGetCommonRequest addCommonRequest;
+    private SubscribeAndBlockRequest subBlockRequest;
+    private RetrieveRequest retrieveRequest;
 
     private List<FriendRelationship> listRelationships;
     private List<String> listEmail;
@@ -63,6 +60,7 @@ public class EmailServiceImpTest {
     private FriendRelationship friendRelationship1;
     private FriendRelationship friendRelationship2;
     private FriendRelationship friendRelationship3;
+    private FriendRelationship friendRelationship4;
     private FriendRelationship blockRelationship1;
     private FriendRelationship subscribeRelationship1;
     private Map<String, Object> body;
@@ -72,16 +70,22 @@ public class EmailServiceImpTest {
     public void setUp() {
         emailRequest = new EmailRequest();
         addCommonRequest = new AddAndGetCommonRequest();
+        subBlockRequest = new SubscribeAndBlockRequest();
+        retrieveRequest = new RetrieveRequest();
+
         emailTest1 = new Email(1L, "hau@gmail.com");
         emailTest2 = new Email(2L, "hoang@gmail.com");
         emailTest3 = new Email(3L, "quan@gmail.com");
         emailTest4 = new Email(4L, "binh@gmail.com");
-        emailTest5 = new Email(5L, "binh@gmail.com");
-        emailTest5 = new Email(5L, "khang@gmail.com");
+        emailTest5 = new Email(5L, "lan@gmail.com");
+        emailTest6 = new Email(5L, "khang@gmail.com");
         friendRelationship1 = new FriendRelationship(1L, 1L, 2L, String.valueOf(FriendStatus.FRIEND), emailTest1, emailTest2);
-        friendRelationship2 = new FriendRelationship(1L, 1L, 3L, String.valueOf(FriendStatus.FRIEND), emailTest1, emailTest2);
-        blockRelationship1 = new FriendRelationship(1L, 4L, String.valueOf(FriendStatus.BLOCK));
-        subscribeRelationship1 = new FriendRelationship(1L, 5L, String.valueOf(FriendStatus.SUBSCRIBE));
+        friendRelationship2 = new FriendRelationship(2L, 1L, 3L, String.valueOf(FriendStatus.FRIEND), emailTest1, emailTest3);
+        friendRelationship3 = new FriendRelationship(3L, 2L, 1L, String.valueOf(FriendStatus.FRIEND), emailTest2, emailTest1);
+        friendRelationship4 = new FriendRelationship(4L, 3L, 1L, String.valueOf(FriendStatus.FRIEND), emailTest3, emailTest1);
+        blockRelationship1 = new FriendRelationship(5L,5L, 4L, String.valueOf(FriendStatus.BLOCK), emailTest5,emailTest4);
+        subscribeRelationship1 = new FriendRelationship(6L,6L, 5L, String.valueOf(FriendStatus.SUBSCRIBE),emailTest6,emailTest5);
+
         listRelationships = new ArrayList<>();
         listEmail = new ArrayList<>();
         body = new HashMap<>();
@@ -103,8 +107,8 @@ public class EmailServiceImpTest {
         responseEntity = emailService.getFriendList(emailRequest);
 
         List<String> emails = (List<String>) responseEntity.getBody().get("friends");
-        assertTrue(responseEntity.getBody().get("success") == "true");
-        assertTrue(responseEntity.getBody().get("count") == (Integer) listRelationships.size());
+        assertSame("true", responseEntity.getBody().get("success"));
+        assertSame(responseEntity.getBody().get("count"), listRelationships.size());
         assertTrue(emails.contains(emailTest2.getEmail()));
         assertTrue(emails.contains(emailTest3.getEmail()));
     }
@@ -112,18 +116,79 @@ public class EmailServiceImpTest {
     @Test
     public void testAddFriendsSuccess() {
         addCommonRequest = new AddAndGetCommonRequest
-                (Arrays.asList(emailTest1.getEmail(),emailTest2.getEmail()));
-        when(emailRepository.findByEmail(emailTest1.getEmail())).thenReturn(Optional.of(emailTest1));
-        when(emailRepository.findByEmail(emailTest2.getEmail())).thenReturn(Optional.of(emailTest2));
+                (Arrays.asList(emailTest1.getEmail(), emailTest2.getEmail()));
+        when(emailRepository.findByEmail(emailTest1.getEmail()))
+                .thenReturn(Optional.of(emailTest1));
+        when(emailRepository.findByEmail(emailTest2.getEmail()))
+                .thenReturn(Optional.of(emailTest2));
         when(relationshipRepository.findByEmailIdAndFriendId
                 (emailTest1.getEmailId(), emailTest2.getEmailId()))
                 .thenReturn(Optional.empty());
-//        when(relationshipRepository.save(friendRelationship1)).thenReturn(friendRelationship1);
-//        when(relationshipRepository.save(friendRelationship2)).thenReturn(friendRelationship2);
         responseEntity = emailService.addFriend(addCommonRequest);
 
-        List<String> emails = (List<String>) responseEntity.getBody().get("friends");
-        assertTrue(responseEntity.getBody().get("success") == "true");
+        assertSame("true", responseEntity.getBody().get("success"));
 
     }
+
+    @Test
+    public void testGetCommonSuccess() {
+        addCommonRequest = new AddAndGetCommonRequest
+                (Arrays.asList(emailTest2.getEmail(), emailTest3.getEmail()));
+        listEmail = Collections.singletonList(emailTest1.getEmail());
+
+        when(emailRepository.findByEmail(emailTest2.getEmail())).thenReturn(Optional.of(emailTest2));
+        when(emailRepository.findByEmail(emailTest3.getEmail())).thenReturn(Optional.of(emailTest3));
+        when(relationshipRepository.findByEmailIdAndStatus
+                (emailTest2.getEmailId(), String.valueOf(FriendStatus.FRIEND)))
+                .thenReturn(Collections.singletonList(friendRelationship3));
+        when(relationshipRepository.findByEmailIdAndStatus
+                (emailTest3.getEmailId(), String.valueOf(FriendStatus.FRIEND))).
+                thenReturn(Collections.singletonList(friendRelationship4));
+        when(emailRepository.findById(emailTest1.getEmailId())).thenReturn(Optional.of(emailTest1));
+
+        responseEntity = emailService.getCommonFriends(addCommonRequest);
+        List<String> emails = (List<String>) responseEntity.getBody().get("friends");
+
+        assertSame("true", responseEntity.getBody().get("success"));
+        assertSame(responseEntity.getBody().get("count"), listEmail.size());
+        assertTrue(emails.contains(emailTest1.getEmail()));
+
+    }
+
+    @Test
+    public void testSubscribeSuccess() {
+        subBlockRequest = new SubscribeAndBlockRequest(emailTest6.getEmail(),emailTest5.getEmail());
+
+        when(emailRepository.findByEmail(emailTest6.getEmail())).thenReturn(Optional.of(emailTest6));
+        when(emailRepository.findByEmail(emailTest5.getEmail())).thenReturn(Optional.of(emailTest5));
+        when(relationshipRepository.findByEmailIdAndFriendId(emailTest6.getEmailId(),emailTest5.getEmailId()))
+                .thenReturn(Optional.empty());
+
+        responseEntity = emailService.subscribeTo(subBlockRequest);
+        assertSame("true", responseEntity.getBody().get("success"));
+    }
+
+    @Test
+    public void testBlockEmailSuccess() {
+        subBlockRequest = new SubscribeAndBlockRequest(emailTest5.getEmail(),emailTest4.getEmail());
+
+        when(emailRepository.findByEmail(emailTest5.getEmail())).thenReturn(Optional.of(emailTest5));
+        when(emailRepository.findByEmail(emailTest4.getEmail())).thenReturn(Optional.of(emailTest4));
+        when(relationshipRepository.findByEmailIdAndFriendId(emailTest5.getEmailId(),emailTest4.getEmailId()))
+                .thenReturn(Optional.empty());
+
+        responseEntity = emailService.blockEmail(subBlockRequest);
+        assertSame("true", responseEntity.getBody().get("success"));
+    }
+
+//    @Test
+//    public void testRetrieveEmailSuccess() {
+//        retrieveRequest = new RetrieveRequest(emailTest5.getEmail(),"Hello "+ emailTest1.getEmail());
+//
+//        when(emailRepository.findByEmail(emailTest5.getEmail())).thenReturn(Optional.of(emailTest5));
+//        when(emailRepository.findById(emailTest1.getEmailId())).thenReturn(Optional.of(emailTest1));
+//
+//        responseEntity = emailService.retrieveEmails(retrieveRequest);
+//        assertSame("true", responseEntity.getBody().get("success"));
+//    }
 }
